70a8d5f55d82f1904fd09c4df4087a292546f0a8
jaj2610 <jaj2610@gmail.com>
2017-12-18 06:31:23 -0500

    Cleanup; small documentation edits

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('lola App', () => {
+describe('seatChart App', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -9,6 +9,6 @@ describe('lola App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('The Seating Chart');
   });
 });
