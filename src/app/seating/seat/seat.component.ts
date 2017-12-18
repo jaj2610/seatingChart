2067a06a786a53182a0f90b93314e6627c59ddb0
jaj2610 <jaj2610@gmail.com>
2017-12-18 03:32:59 -0500

    Add styling; Move selectSeat to selectedService
     - simple styling for seat options
    
     - selectSeat in selectedService; prevent selection of occupied seats
    
     - add tooltip with basic info on seats

--- src/app/seating/seat/seat.component.ts
+++ src/app/seating/seat/seat.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Seat } from './seat.interface';
 
 @Component({
   selector: 'app-seat',
@@ -6,12 +7,38 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./seat.component.css']
 })
 export class SeatComponent implements OnInit {
-  @Input() seat = {};
-  @Input() selected = false;
+  @Input() seat: Seat;
+
+  private _selected = false;
+
+  @Input() public set selected(val) {
+    this.styles['selected'] = val;
+    this._selected = val;
+  }
+
+  public get selected() {
+    return this._selected;
+  }
+
+  public tooltip = '';
+
+  styles = {};
 
   constructor() { }
 
   ngOnInit() {
+    // set styles based on seat values
+    this.styles = {
+      'premium': this.seat.premium,
+      'occupied': this.seat.occupied,
+      'overWing': this.seat.overWing,
+      'selected': this._selected
+    };
+
+    this.tooltip = 'Seat ' + this.seat.row + this.seat.seat + ': ' + this.seat.class + ' class';
+    if (this.seat.premium) {
+      this.tooltip += ' (premium)';
+    }
   }
 
 }
--- src/app/seating/shared/selected.service.ts
+++ src/app/seating/shared/selected.service.ts
@@ -7,4 +7,12 @@ export class SelectedService {
 
   constructor() { }
 
+  public selectSeat(seat) {
+    if (!seat.occupied) {
+       this.selectedSeat = seat;
+       console.log('Seat selected:', seat.seat, seat.row);
+    } else {
+      console.log('Seat occupied:', seat.seat, seat.row);
+    }
+  }
 }
