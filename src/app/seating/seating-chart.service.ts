752f49f3e36a0e172bdde555bad02b02b3ce1ca3
jaj2610 <jaj2610@gmail.com>
2017-12-18 02:26:00 -0500

    Fill out seating-chart.service to organize data
     - get seating data from seating-data.service
    
     - manipulate data into sorted rows and columns
    
     - make seatingChart accessible to components

--- src/app/seating/seating-chart.service.ts
+++ src/app/seating/seating-chart.service.ts
@@ -1,8 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Response } from '@angular/http';
+
+import { SeatingDataService } from './seating-data.service';
 
 @Injectable()
 export class SeatingChartService {
 
-  constructor() { }
+  public seatingData = [];
+  public seatingChart = [[]];
 
+  constructor(public seatingDataService: SeatingDataService) {
+  }
+
+  public initChart() {
+    this.seatingDataService.getSeatingData().subscribe(data => {this.seatingData = data; this.sortRows()}, error => console.log(error));
+  }
+
+  private sortRows() {
+    // create array of rows containing all seats in each row
+    this.seatingData.forEach(element => {
+      this.seatingChart[element.row] = this.seatingChart[element.row] || [];
+      this.seatingChart[element.row].push(element);
+    });
+
+    this.sortColumns();
+  }
+
+  private sortColumns() {
+    // sort columns (by seat) within rows
+    this.seatingChart.forEach(row => {
+      row.sort(function(a, b) {
+        return a.seat > b.seat ? 1 : -1;
+      });
+    })
+  }
 }
